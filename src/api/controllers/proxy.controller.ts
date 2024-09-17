1966164e9cd5535b79eba9ea7969e2eec7eacf68
Judson Cairo <judsonjuniorr@gmail.com>
2024-09-17 12:46:33 -0300

    Fix error logging in testProxy method

--- src/api/controllers/proxy.controller.ts
+++ src/api/controllers/proxy.controller.ts
@@ -54,10 +54,9 @@ export class ProxyController {
     } catch (error) {
       if (axios.isAxiosError(error) && error.response?.data) {
         logger.error('testProxy error: ' + error.response.data);
-      } else if (axios.isAxiosError(error)) {
-        logger.error('testProxy error: ');
       } else {
         logger.error('testProxy error: ');
+        logger.error(error.message);
       }
       return false;
     }
